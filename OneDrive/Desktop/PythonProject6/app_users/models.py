3014f2a683a14c2792c211e47f60ec8c7396c59d
tako19-zuko <takotabatadze87@gmail.com>
2025-10-16 22:11:34 +0400

    full

--- OneDrive/Desktop/PythonProject6/app_users/models.py
+++ OneDrive/Desktop/PythonProject6/app_users/models.py
@@ -91,4 +91,4 @@ class PasswordResetToken(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    is_used = models.BooleanField(default=False)
+    is_used = models.BooleanField(default=False)  
